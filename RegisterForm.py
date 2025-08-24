9b67d6d655f37db0feefa8c1bf5f617ccd6f0f3b
Riza188 <riza.1882006@gmail.com>
2025-08-24 19:01:50 +0530

    Create RegisterForm.py

---
+++ RegisterForm.py
@@ -0,0 +1,12 @@
+# register form
+
+users = []  # store all registered users here
+
+def register_user(name, email, password):
+    user = {
+        "name": name,
+        "email": email,
+        "password": password
+    }
+    users.append(user)
+    return "User registered successfully!"
